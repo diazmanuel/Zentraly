65220d0392e50c93942e3dffd10070cff209d8f0
zentralySAU <157702448+zentralySAU@users.noreply.github.com>
2026-09-17 13:59:42 +0000

    Preserve persisted Zentraly versions on partial readings

--- homeassistant/components/zentraly/__init__.py
+++ homeassistant/components/zentraly/__init__.py
@@ -29,6 +29,7 @@ from homeassistant.exceptions import (
 from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.helpers.typing import UNDEFINED
 
 from .const import DOMAIN, SCAN_INTERVAL
 from .models import ZentralyConfigEntry, ZentralyData, ZentralyDevice
@@ -120,8 +121,8 @@ async def _async_refresh_device_info(
 
     device_registry.async_update_device(
         registry_device_id,
-        sw_version=device.firmware_version,
-        hw_version=device.hardware_version,
+        sw_version=firmware_version if firmware_version is not None else UNDEFINED,
+        hw_version=hardware_version if hardware_version is not None else UNDEFINED,
     )
 
     _LOGGER.debug(
@@ -291,6 +292,7 @@ async def async_setup_entry(
         api.add_authentication_error_listener(lambda: entry.async_start_reauth(hass))
     )
 
+    # Connection failures are retried inside the library's background task.
     await api.async_connect()
 
     entry.runtime_data = runtime_data
